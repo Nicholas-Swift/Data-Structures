d38e7d3bda87ea6df5e3524303fd9e3eebda47da
Nicholas Swift <nicholas.w.swift@gmail.com>
2017-01-25 20:48:15 -0800

    Recursion done, search working on it

--- search.py
+++ search.py
@@ -1,11 +1,13 @@
 #!python
 
+import math
+
 def linear_search(array, item):
     """return the first index of item in array or None if item is not found"""
     # implement linear_search_iterative and linear_search_recursive below, then
     # change this to call your implementation to verify it passes all tests
-    return linear_search_iterative(array, item)
-    # return linear_search_recursive(array, item)
+    #return linear_search_iterative(array, item)
+    return linear_search_recursive(array, item)
 
 
 def linear_search_iterative(array, item):
@@ -17,10 +19,12 @@ def linear_search_iterative(array, item):
 
 
 def linear_search_recursive(array, item, index=0):
-    # TODO: implement linear search recursively here
-    pass
-    # once implemented, change linear_search to call linear_search_recursive
-    # to verify that your recursive implementation passes all tests below
+
+    # Base case
+    if array[index] == item:
+        return index
+
+    return linear_search_recursive(array, item, index + 1)
 
 
 def binary_search(array, item):
@@ -32,10 +36,39 @@ def binary_search(array, item):
 
 
 def binary_search_iterative(array, item):
-    # TODO: implement binary search iteratively here
-    pass
-    # once implemented, change binary_search to call binary_search_iterative
-    # to verify that your iterative implementation passes all tests below
+    # Binary search iteratively
+
+    # Base cases
+    if not array:
+        return None
+    
+    
+
+    # Iterate through
+    while len(array) > 1:
+
+        # Set to middle of array
+        index = int(math.floor(len(array)/2))
+
+        # Found item, return
+        if item == array[index]:
+            return item
+
+        # Too low, slice to upper half of array
+        if item > array[index]:
+            array = array[index:]
+
+        # Too high, slice to lower half of array
+        elif item < array[index]:
+            array = array[:index]
+
+        print(array)
+
+    # if len(array) == 1 and item == array[0]:
+    #     return item
+
+    # Did not find
+    return None
 
 
 def binary_search_recursive(array, item, left=None, right=None):
@@ -43,3 +76,5 @@ def binary_search_recursive(array, item, left=None, right=None):
     pass
     # once implemented, change binary_search to call binary_search_recursive
     # to verify that your recursive implementation passes all tests below
+
+print(binary_search([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 1))
