b973ae8ef8c59eb6238e4fca630d852531aa2905
Nicholas Swift <nicholas.w.swift@gmail.com>
2017-02-06 13:09:31 -0800

    Binary search tree with tests delete not working fully yet

---
+++ binarysearchtree.py
@@ -0,0 +1,149 @@
+# #!python
+
+class Node(object):
+
+    def __init__(self, data):
+        """Initialize this node with the given data"""
+        self.data = data
+        self.left = None
+        self.right = None
+
+    def __repr__(self):
+        """Return a string representation of this node"""
+        return 'Node({})'.format(repr(self.data))
+
+
+class BinarySearchTree(object):
+
+    def __init__(self, iterable=None):
+        """Initialize this binary search tree and insert the given items if any"""
+        self.root = None
+        self.size = 0
+        if iterable:
+            for item in iterable:
+                self.insert(item)
+
+    def length(self):
+        """Return the length of this binary search tree"""
+        return self.size
+
+    def is_empty(self):
+        """Return True if this binary search tree contains no nodes"""
+        return self.length() == 0
+
+    def search(self, item):
+        """Search through the binary search"""
+        current = self.root
+        while current is not None:
+            if item == current.data:
+                return True
+            if item > current.data:
+                current = current.right
+            else:
+                current = current.left
+        return False
+
+    def insert(self, item):
+        """Insert a new item into the binary search tree, or assert ValueError if item already exists"""
+        if self.root is None:
+            self.root = Node(item)
+            self.size += 1
+            return
+
+        previous = self.root
+        current = self.root
+        while current is not None:
+            if item == current.data:
+                assert ValueError("Can not add the same item to a binary search tree")
+            if item > current.data:
+                previous = current
+                current = current.right
+            else:
+                previous = current
+                current = current.left
+        if item > previous.data:
+            previous.right = Node(item)
+        else:
+            previous.left = Node(item)
+        self.size += 1
+
+    def delete(self, item):
+        """Delete an item from the binary search tree, or assert ValueError if item does not exit"""
+
+        if self.root is None:
+            assert ValueError("Can not delete from an empty binary search tree")
+
+        if item == self.root.data:
+
+            # Search rightmost of left
+            if self.root.left is not None:
+                previous = self.root.left
+                current = self.root.left
+                while current.right is not None:
+                    previous = current
+                    current = current.right
+                if previous != self.root:
+                    previous.right = current.right
+                    previous.left = current.left
+                current.right = self.root.right
+                current.left = self.root.left
+                self.root = current
+                self.size -= 1
+                return
+
+            # Search leftmost of right
+            elif self.root.right is not None:
+                previous = self.root.right
+                current = self.root.right
+                while current.left is not None:
+                    previous = current
+                    current = current.left
+                if previous != self.root:
+                    previous.right = current.right
+                    previous.left = current.left
+                current.right = self.root.right
+                current.left = self.root.left
+                self.root = current
+                self.size -= 1
+                return
+
+            # No left or right, no more tree!
+            self.root = None
+            self.size -= 1
+            return
+
+        # Not the root node
+        previous = self.root
+        current = self.root
+        while current is not None:
+            if item == current.data:
+                if item > previous.data:
+                    previous.right = current.right
+                else:
+                    previous.left = current.left
+                self.size -= 1
+                return
+            if item > current.data:
+                previous = current
+                current = current.right
+            else:
+                previous = current
+                current = current.left
+        assert ValueError("Item does not exist in the binary search tree")
+
+
+def main():
+    bst = BinarySearchTree()
+    bst.insert(5)
+    bst.insert(3)
+    bst.insert(4)
+    bst.insert(2)
+    bst.insert(50)
+    bst.insert(49)
+    bst.delete(5)
+
+
+if __name__ == '__main__':
+    main()
+
+
---
+++ test_binarysearchtree.py
@@ -0,0 +1,85 @@
+#!python
+
+from binarysearchtree import BinarySearchTree, Node
+import unittest
+
+
+class NodeTest(unittest.TestCase):
+
+    def test_init(self):
+        data = 'ABC'
+        node = Node(data)
+        assert node.data is data
+        assert node.right is None
+        assert node.left is None
+
+
+class LinkedListTest(unittest.TestCase):
+
+    def test_init(self):
+        bst = BinarySearchTree()
+        assert bst.root is None
+        assert bst.is_empty() is True
+
+    def test_init_with_list(self):
+        bst = BinarySearchTree(['B', 'A', 'C'])
+        assert bst.root.data == 'B'
+        assert bst.root.right.data == 'C'
+        assert bst.root.left.data == 'A'
+        assert bst.is_empty() is False
+
+    def test_length(self):
+        bst = BinarySearchTree()
+        assert bst.length() == 0
+        bst.insert('A')
+        assert bst.length() == 1
+        bst.insert('B')
+        assert bst.length() == 2
+        bst.insert('C')
+        assert bst.length() == 3
+        bst.delete('C')
+        assert bst.length() == 2
+        bst.delete('B')
+        assert bst.length() == 1
+        bst.delete('A')
+        assert bst.length() == 0
+
+    def test_search(self):
+        bst = BinarySearchTree(['A', 'B', 'C'])
+        assert bst.search('A') == True
+        assert bst.search('B') == True
+        assert bst.search('C') == True
+        assert bst.search('D') == False
+
+    def test_insert(self):
+        bst = BinarySearchTree()
+        bst.insert(10)
+        assert bst.root.data == 10
+        bst.insert(5)
+        assert bst.root.data == 10
+        assert bst.root.left.data == 5
+        bst.insert(7)
+        assert bst.root.data == 10
+        assert bst.root.left.data == 5
+        assert bst.root.left.right.data == 7
+
+    def test_delete(self):
+        bst = BinarySearchTree([50, 20, 19, 25, 28, 59, 58, 60])
+        bst.delete(60)
+        assert bst.root.data == 50
+        assert bst.root.right.right == None
+        assert bst.search(60) == False
+        bst.delete(20)
+        assert bst.root.data == 50
+        assert bst.root.left.data == 19
+        assert bst.root.left.right.data == 25
+        assert bst.search(20) == False
+        bst.delete(50)
+        assert bst.root.data == 28
+        assert bst.root.right.data == 59
+        assert bst.root.left.data == 25
+        assert bst.search(50) == False
+
+
+if __name__ == '__main__':
+    unittest.main()
