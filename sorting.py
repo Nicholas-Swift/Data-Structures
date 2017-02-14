e6cb64c17a27de372fee40245594840df8b64743
Nicholas Swift <nicholas.w.swift@gmail.com>
2017-02-13 21:02:53 -0800

    in place counting sort

--- sorting.py
+++ sorting.py
@@ -35,21 +35,52 @@ def insertion_sort(arr):
 
     current_index = 0
     for i in range(len(arr)):
-
-        end = True
         for j in range(0, current_index):
             if arr[i] < arr[j]:
                 arr.insert(j, arr.pop(i))
-                end = False
                 break
-        if end:
-            arr.insert(current_index, arr.pop(i))
         current_index += 1
+
+def counting_sort(arr):
+
+    # Find min and max
+    min_num = None
+    max_num = None
+    for num in arr:
+        if num < min_num or min_num is None:
+            min_num = num
+        if num > max_num or max_num is None:
+            max_num = num
+
+    # Create a histogram
+    histogram = [None] * (max_num - min_num + 1)
+
+    # Add to buckets
+    for num in arr:
+        if histogram[num - min_num] is None:
+            histogram[num - min_num] = (num, 1)
+        else:
+            histogram[num - min_num] = (num, histogram[num - min_num][1] + 1)
+
+    # Overwrite array
+    index = 0
+    for item in histogram:
+        if item is None:
+            continue
+        else:
+            for num in range(item[1]):
+                arr[index] = item[0]
+                index += 1
+
+def bucket_sort(arr):
+
+    buckets = [] * len(arr)
+    pass
 
 
 def main():
-    arr = [700, 1, 5, 3, 7, 4, 9, 4, 3, 700]
-    insertion_sort(arr)
+    arr = [18, 5, 3, 7, 4, 9, 4, 3, 6]
+    counting_sort(arr)
     print(arr)
 
 
